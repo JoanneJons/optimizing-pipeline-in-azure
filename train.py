0813daeac54edbbcaea6bba983492a7ecbe6ceec
Joanne Mary Jons <41639224+JoanneJons@users.noreply.github.com>
2021-01-01 15:54:50 +0530

    Add files via upload
    Train.py set up, Project file updated with compute cluster creation, parameter sampler and early termination policy

--- train.py
+++ train.py
@@ -9,18 +9,23 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+web_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+### ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files(web_path)
+#ds = Dataset.Tabular.from_delimited_files(web_path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+### YOUR CODE HERE ###
+x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
 run = Run.get_context()
 
@@ -49,7 +54,8 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
+
 
 def main():
     # Add arguments to script
