8ac3bfd663e3a804437ab85ef012d9af28942c55
Manoj Jons <mjons@cognisonus.com>
2021-01-02 18:16:17 +0530

    HyperDrive Fixed

--- train.py
+++ train.py
@@ -11,24 +11,6 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Dataset
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-web_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
-### ds = ### YOUR CODE HERE ###
-ds = TabularDatasetFactory.from_delimited_files(web_path)
-#ds = Dataset.Tabular.from_delimited_files(web_path)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###
-x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -56,6 +38,25 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
 
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+web_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+### ds = ### YOUR CODE HERE ###
+# ds = TabularDatasetFactory.from_delimited_files(web_path)
+#ds = Dataset.Tabular.from_delimited_files(web_path)
+
+# x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###
+# x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+# run = Run.get_context()
+
+
 
 def main():
     # Add arguments to script
@@ -66,13 +67,24 @@ def main():
 
     args = parser.parse_args()
 
+    ds = TabularDatasetFactory.from_delimited_files(path=web_path)
+    x, y = clean_data(ds)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+    run = Run.get_context()
+
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(value=model, filename='outputs/model.pkl')
 
 if __name__ == '__main__':
     main()
